ac553020a6ac74486870d8d703da5078e8673621
Isaac Tan <iztan98@gmail.com>
2024-03-08 17:19:10 +0800

    Update waifu.vue

--- pages/waifu.vue
+++ pages/waifu.vue
@@ -1,21 +1,26 @@
 <template>
-    <div v-if="waifu" class="">
-        <UContainer>
-            <div class="flex flex-col items-center">
-                <h1 class="text-white text-4xl">Waifu</h1>
-                <div class="mt-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-                    <UButton v-for="(w, i) in waifu.files" :key="i" class="relative" @click="openModal(w)" color="purple"
-                        variant="ghost">
-                        <img :src="w" loading="lazy" alt="" class="rounded-lg object-cover w-56 h-64">
-                    </UButton>
+    <div class="min-h-screen">
+        <h1 class="text-center mt-3 text-white text-4xl">Waifu</h1>
+        <div v-if="waifu">
+            <UContainer>
+                <div class="flex flex-col items-center">
+                    <div class="mt-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+                        <UButton v-for="(w, i) in waifu.files" :key="i" class="relative" @click="openModal(w)"
+                            color="purple" variant="ghost">
+                            <img :src="w" loading="lazy" alt="" class="rounded-lg object-cover w-56 h-64">
+                        </UButton>
+                    </div>
                 </div>
-            </div>
-        </UContainer>
-        <UModal v-model="isOpen" :ui="{ overlay: { background: 'bg-zinc-800/80' } }">
-            <div class="p-1">
-                <img :src="showImg" alt="" class="rounded-lg object-cover">
-            </div>
-        </UModal>
+            </UContainer>
+            <UModal v-model="isOpen" :ui="{ overlay: { background: 'bg-zinc-800/80' } }">
+                <div class="p-1">
+                    <img :src="showImg" alt="" class="rounded-lg object-cover">
+                </div>
+            </UModal>
+        </div>
+        <div v-else class="w-full flex justify-center mt-5">
+            <SpiningLoading></SpiningLoading>
+        </div>
     </div>
 </template>
 
@@ -30,7 +35,8 @@ export default {
         }
     },
     async mounted() {
-        await fetch('https://api.waifu.pics/many/sfw/waifu', {
+        const params = this.$route.query.type ?? 'sfw'
+        await fetch(`https://api.waifu.pics/many/${params}/waifu`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
