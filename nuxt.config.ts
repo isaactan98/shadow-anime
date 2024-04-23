184fe4d84d9e5331992503470423004ed0097ce2
Isaac Tan <iztan98@gmail.com>
2024-04-23 22:26:13 +0800

    update: bg color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "theme-color", content: "#0d0d12" },
+        { name: "theme-color", content: "#1a1a1a" },
         {
           hid: "description",
           name: "description",
