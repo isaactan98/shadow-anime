7026cd470ed97542f35609dcbba124f01ddff94e
Isaac Tan <iztan98@gmail.com>
2024-05-01 11:35:59 +0800

    Update [anime].vue

--- pages/anime/[anime].vue
+++ pages/anime/[anime].vue
@@ -319,10 +319,17 @@ export default {
         const mapping = this.animeMeta.mappings?.find((mapping) => mapping.providerId === 'tmdb');
         const gogoAnime = this.animeMeta.mappings?.find((mapping) => mapping.providerId === 'gogoanime');
         if (gogoAnime == null) {
-            const synonyms = this.animeMeta.synonyms.length > 0 ? this.animeMeta.synonyms[0] : this.animeMeta.title.english ?? this.animeMeta.title.romaji;    
-            await searchGogoanime(synonyms).then(async (d) => {
+            const title = this.animeMeta.title.native ?? this.animeMeta.title.romaji;
+            await searchGogoanime(title).then(async (d) => {
                 if (d.results.length > 0) {
                     this.anime = await this.getAnime(config, d.results[0].id);
+                } else {
+                    const synonyms = this.animeMeta.synonyms.length > 0 ? this.animeMeta.synonyms[0] : this.animeMeta.title.english ?? this.animeMeta.title.romaji;
+                    searchGogoanime(synonyms).then(async (d) => {
+                        if (d.results.length > 0) {
+                            this.anime = await this.getAnime(config, d.results[0].id);
+                        }
+                    });
                 }
             });
         }
