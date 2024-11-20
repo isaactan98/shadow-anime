1f691d9c4bedc94377511bc6f80d0eab25d0c070
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2024-11-21 07:49:35 +0800

    Update VideoPlayer.vue

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,31 +1,34 @@
 <template>
-    <media-player :title="title" :poster="poster" :src="src" aspect-ratio="16/9" crossorigin>
+    <media-player :title="title" :poster="poster" :src="cors + src" aspect-ratio="16/9" crossorigin>
         <media-outlet></media-outlet>
         <media-community-skin></media-community-skin>
     </media-player>
 </template>
 
 <script>
-import 'vidstack/styles/defaults.css'
-import 'vidstack/styles/community-skin/video.css'
+import "vidstack/styles/defaults.css";
+import "vidstack/styles/community-skin/video.css";
 
-import { defineCustomElements } from 'vidstack/elements';
+import { defineCustomElements } from "vidstack/elements";
 
 export default {
-    prop: {
-        src: null,
-        title: null,
-        poster: null
-    },
-    data() {
-        return {
-            src: null,
-            title: null,
-            poster: null
-        }
-    },
-    mounted() {
-        defineCustomElements();
-    }
-}
+	prop: {
+		src: null,
+		title: null,
+		poster: null,
+	},
+	data() {
+		return {
+			src: null,
+			title: null,
+			poster: null,
+			cors: null,
+		};
+	},
+	mounted() {
+		const config = useRuntimeConfig();
+		this.cors = config["public"].corsApi;
+		defineCustomElements();
+	},
+};
 </script>
